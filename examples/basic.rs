2ec05a072799523e0865badbe3a7ca306278e1e9
jice <jice.nospam@gmail.com>
2023-01-13 15:21:40 +0100

    basic example only exits when pressing ESC

--- examples/basic.rs
+++ examples/basic.rs
@@ -23,16 +23,14 @@ fn main() {
         intercept_close_request: false,
     });
     // start game loop
+    uni_app::App::print("Press Escape to exit");
     app.run(move |app: &mut uni_app::App| {
         for evt in app.events.borrow().iter() {
             // print on stdout (native) or js console (web)
-            uni_app::App::print(format!("{:?}\n", evt));
+            uni_app::App::print(format!("{:?}", evt));
             // exit on key or mouse press
-            match evt {
-                &uni_app::AppEvent::KeyUp(_) => {
-                    uni_app::App::exit();
-                }
-                &uni_app::AppEvent::MouseUp(_) => {
+            match &evt {
+                uni_app::AppEvent::KeyUp(ev) if ev.code == "Escape" => {
                     uni_app::App::exit();
                 }
                 _ => (),
